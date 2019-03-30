61b08719c377b41f508522291283eb2fc6eb39b9
Dylan Bulmer <dylanmbulmer@gmail.com>
2019-03-30 19:35:51 -0400

    debugging

--- Server/server.js
+++ Server/server.js
@@ -44,6 +44,7 @@ app.post('/', function (req, res) {
 // Login to the docs
 app.post('/login', function (req, res) {
     let { username, password } = req.body;
+    console.log(username + " is logging in");
     db.login(username, password, function (data) {
         res.json(data);
     });
