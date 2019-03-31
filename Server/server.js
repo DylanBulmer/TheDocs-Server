09ee21e67c968a55c6fbe5a976357f4eba65acd3
Dylan Bulmer <dylanmbulmer@gmail.com>
2019-03-31 15:36:47 -0400

    fixed https post requests?

--- Server/server.js
+++ Server/server.js
@@ -27,7 +27,7 @@ const db = require('./modules/database.js');
 // Make the server use things
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
